c1ade410d0e1bec105196ab163b96b80c7f4cccd
linux_china <libing.chen@gmail.com>
2022-08-16 12:53:26 -0700

    feat: add my_ip2 as example

--- httpfile-demo/src/httpbin.rs
+++ httpfile-demo/src/httpbin.rs
@@ -1,2 +1,7 @@
+use reqwest::{Response, Result};
 // this would include code generated for package hello from .http file
 httpfile::include_http!("index");
+
+pub async fn my_ip2() -> Result<Response> {
+    reqwest::get("https://httpbin.org/ip").await
+}
