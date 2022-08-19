6e81245c0abda8ce1e8de8b71bd5a8b0137328d5
linux_china <libing.chen@gmail.com>
2022-08-18 22:17:35 -0700

    chore: remove imports

--- httpfile-demo/src/httpbin.rs
+++ httpfile-demo/src/httpbin.rs
@@ -1,5 +1,2 @@
-use std::collections::HashMap;
-use reqwest::{Response, Result};
-use reqwest::header::HeaderMap;
 // this would include code generated for package hello from .http file
 httpfile::include_http!("index");
