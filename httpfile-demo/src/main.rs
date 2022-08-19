a158572e7910ced30213654d4111a3778e6721de
linux_china <libing.chen@gmail.com>
2022-08-18 22:17:58 -0700

    chore: rename request name

--- httpfile-demo/src/main.rs
+++ httpfile-demo/src/main.rs
@@ -10,8 +10,7 @@ struct MyIp {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    httpbin::hello();
-    let json: MyIp = httpbin::my_ip2().await?.json().await?;
+    let json: MyIp = httpbin::my_ip().await?.json().await?;
     println!("{:?}", json);
     Ok(())
 }
